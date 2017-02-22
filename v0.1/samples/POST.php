36229c7552425c10e57b80c658640fbb9c49ded2
kazlauskis <karolis@kazlauskis.com>
2017-02-22 16:49:30 +0000

    Enable the submission of image

--- v0.1/samples/POST.php
+++ v0.1/samples/POST.php
@@ -6,7 +6,7 @@
 function indicia_api_samples_post() {
   indicia_api_log('Samples POST');
   indicia_api_log(print_r($_POST, 1));
-  $submission = json_decode($_POST['submission'], true);
+  $submission = json_decode($_POST['submission'], TRUE);
 
   if (!validate_samples_post_request($submission)) {
     return;
@@ -22,9 +22,6 @@ function indicia_api_samples_post() {
     return;
   }
 
-  // Construct photos.
-  process_files();
-
   // Construct post parameter array.
   $submission = process_parameters($submission, $connection);
 
@@ -34,10 +31,10 @@ function indicia_api_samples_post() {
   }
 
   // Send record to indicia.
-  $response = data_entry_helper::forward_post_to('save', $submission, $auth['write_tokens']);
+  $response = forward_post_to('save', $submission, $auth['write_tokens']);
 
   // Return response to client.
-  return_response($response, $submission);
+  return_response($response);
 }
 
 /**
@@ -51,8 +48,8 @@ function process_files() {
       $baseModel = empty($matches['sample']) ? 'occurrence' : 'sample';
       $name = "$baseModel:image:$matches[id]";
       // Mobile generated files can have file name in format
-      // resize.jpg?1333102276814 which will fail the warehouse submission
-      // process.
+      // Resize.jpg?1333102276814 which will fail the warehouse submission
+      // Process.
       if (strstr($info['type'], 'jpg') !== FALSE || strstr($info['type'], 'jpeg') !== FALSE) {
         $info['name'] = uniqid() . '.jpg';
       }
@@ -63,10 +60,10 @@ function process_files() {
     }
     // Handle files sent along with a species checklist style submission.
     // Files should be POSTed in
-    // a field called sc:<gridrow>::photo[1-9] and will then get moved to the
-    // interim image folder and
-    // linked to the form using a field called
-    // sc:<gridrow>::occurremce_media:path:[1-9] .
+    // A field called sc:<gridrow>::photo[1-9] and will then get moved to the
+    // Interim image folder and
+    // Linked to the form using a field called
+    // Sc:<gridrow>::occurremce_media:path:[1-9] .
     elseif (preg_match('/^sc:(?P<gridrow>.+)::photo(?P<id>[0-9])$/', $name, $matches)) {
       $interim_image_folder = isset(data_entry_helper::$interim_image_folder) ? data_entry_helper::$interim_image_folder : 'upload/';
       $uploadPath = data_entry_helper::relative_client_helper_path() . $interim_image_folder;
@@ -133,6 +130,13 @@ function process_parameters($submission, $connection) {
     ]);
   }
 
+  foreach ($submission['media'] as $media) {
+    array_push($model['subModels'], [
+      'fkId' => 'sample_id',
+      'model' => process_media_parameters($media, FALSE),
+    ]);
+  }
+
   return $model;
 }
 
@@ -140,6 +144,7 @@ function process_occurrence_parameters($submission, $connection) {
   $model = [
     "id" => "occurrence",
     "fields" => [],
+    "subModels" => [],
   ];
 
   foreach ($submission['fields'] as $key => $field) {
@@ -161,6 +166,31 @@ function process_occurrence_parameters($submission, $connection) {
     $model['fields']['external_key'] = ['value' => $submission['external_key']];
   }
 
+  foreach ($submission['media'] as $media) {
+    array_push($model['subModels'], [
+      'fkId' => 'occurrence_id',
+      'model' => process_media_parameters($media),
+    ]);
+  }
+
+  return $model;
+}
+
+
+function process_media_parameters($submission, $occurrence = TRUE) {
+  $model = [
+    "id" => $occurrence ? "occurrence_medium" : 'sample_medium',
+    "fields" => [],
+  ];
+
+  // Generate new name.
+  $ext = $_FILES[$submission['name']]['type'] === 'image/png' ? '.png' : '.jpg';
+  $newName = bin2hex(openssl_random_pseudo_bytes(20)) . $ext;
+
+  $_FILES[$submission['name']]['name'] = $newName;
+
+  $model['fields']['path'] = ['value' => $newName];
+
   return $model;
 }
 
@@ -180,11 +210,14 @@ function has_duplicates($submission) {
   if (count($duplicates) > 0) {
     $errors = [];
     foreach ($duplicates as $duplicate) {
+      // TODO: get actual sample external_key,
+      // because this could be a subsample occurrence
       array_push($errors, [
         'status' => '409',
         'id' => $duplicate['id'],
         'external_key' => $duplicate['external_key'],
         'sample_id' => $duplicate['sample_id'],
+        'sample_external_key' => $submission['fields']['external_key']['value'],
         'title' => 'Occurrence already exists.',
       ]);
     }
@@ -234,7 +267,7 @@ function find_duplicates($submission) {
  */
 function validate_samples_post_request($submission) {
   // Reject submissions with an incorrect secret (or instances where secret is
-  // not set).
+  // Not set).
   if (!indicia_api_authorise_key()) {
     error_print(401, 'Unauthorized', 'Missing or incorrect API key');
 
@@ -254,19 +287,86 @@ function validate_samples_post_request($submission) {
     return FALSE;
   }
 
-  // Validate core fields.
-  if (!$submission['fields']['date']) {
-    error_print(400, 'Bad Request', 'Missing date.');
+  // Validate sample.
+  if (!validate_sample($submission)) {
+    return FALSE;
+  }
+
+  return TRUE;
+}
+
+function validate_sample($model) {
+  if (!$model['fields']['date']) {
+    error_print(400, 'Bad Request', 'Missing sample date.');
 
     return FALSE;
   }
-  if (!$submission['fields']['entered_sref']) {
-    error_print(400, 'Bad Request', 'Missing entered_sref.');
+  if (!$model['fields']['entered_sref']) {
+    error_print(400, 'Bad Request', 'Missing sample entered_sref.');
 
     return FALSE;
   }
-  if (!$submission['fields']['entered_sref_system']) {
-    error_print(400, 'Bad Request', 'Missing entered_sref_system.');
+  if (!$model['fields']['entered_sref_system']) {
+    error_print(400, 'Bad Request', 'Missing sample entered_sref_system.');
+
+    return FALSE;
+  }
+
+  if (!empty($model['samples'])) {
+    foreach ($model['samples'] as $sample) {
+      if (!validate_sample($sample)) {
+        return FALSE;
+      }
+    }
+  }
+
+  if (!empty($model['occurrences'])) {
+    foreach ($model['occurrences'] as $occurrence) {
+      if (!validate_occurrence($occurrence)) {
+        return FALSE;
+      }
+    }
+  }
+
+  if (!empty($model['media'])) {
+    foreach ($model['media'] as $media) {
+      if (!validate_media($media)) {
+        return FALSE;
+      }
+    }
+  }
+
+  return TRUE;
+}
+
+function validate_occurrence($model) {
+  if (!$model['fields']['taxa_taxon_list_id']) {
+    error_print(400, 'Bad Request', 'Missing occurrence taxa_taxon_list_id.');
+
+    return FALSE;
+  }
+
+  if (!empty($model['media'])) {
+    foreach ($model['media'] as $media) {
+      if (!validate_media($media)) {
+        return FALSE;
+      }
+    }
+  }
+
+  return TRUE;
+}
+
+
+function validate_media($model) {
+  if (!$model['name']) {
+    error_print(400, 'Bad Request', 'Missing media name.');
+
+    return FALSE;
+  }
+
+  if (!$_FILES[$model['name']]) {
+    error_print(400, 'Bad Request', 'Missing media.');
 
     return FALSE;
   }
@@ -274,9 +374,7 @@ function validate_samples_post_request($submission) {
   return TRUE;
 }
 
-
-// todo: remove submission param once the server returns external keys
-function return_response($response, $submission) {
+function return_response($response) {
   if (isset($response['error'])) {
     $errors = [];
     foreach ($response['errors'] as $key => $error) {
@@ -290,28 +388,232 @@ function return_response($response, $submission) {
   else {
     // Created.
     drupal_add_http_header('Status', '201 Created');
-    $data = [
-      'type' => 'samples',
-      'id' => (int) $response['struct']['id'],
-      'external_key' => $submission['fields']['external_key']['value'],
-      'subModels' => [],
-    ];
-
-    // Occurrences only
-    // todo: subModel samples
-    foreach ($response['struct']['children'] as $subModel) {
-      if ($subModel['model'] === 'occurrence') {
-        array_push($data['subModels'], [
-          'type' => 'occurrence',
-          'id' => (int) $subModel['id'],
-          // todo: extract from response once the warehouse supports it
-          'external_key' => $submission['subModels'][0]['model']['fields']['external_key']['value'],
-        ]);
-      }
-    }
+    $data = extract_sample_response($response['struct']);
 
     $output = ['data' => $data];
     drupal_json_output($output);
     indicia_api_log(print_r($response, 1));
   }
+}
+
+function extract_sample_response($model) {
+  $data = [
+    'id' => (int) $model['id'],
+    'external_key' => $model['external_key'],
+    'created_on' => $model['created_on'],
+    'updated_on' => $model['updated_on'],
+  ];
+
+  foreach ($model['children'] as $child) {
+    switch ($child['model']) {
+      case 'occurrence':
+        $data['occurrences'] = empty($data['occurrences']) ? [] : $data['occurrences'];
+        array_push($data['occurrences'], extract_occurrence_response($child));
+        break;
+
+      case 'sample':
+        $data['samples'] = empty($data['samples']) ? [] : $data['samples'];
+        array_push($data['samples'], extract_sample_response($child));
+        break;
+
+      case 'sample_medium':
+        $data['media'] = empty($data['media']) ? [] : $data['media'];
+        array_push($data['media'], extract_media_response($child));
+        break;
+
+      default:
+    }
+  }
+  return $data;
+}
+
+function extract_occurrence_response($model) {
+  $data = [
+    'id' => (int) $model['id'],
+    'external_key' => $model['external_key'],
+    'created_on' => $model['created_on'],
+    'updated_on' => $model['updated_on'],
+  ];
+
+  foreach ($model['children'] as $child) {
+    switch ($child['model']) {
+      case 'occurrence_medium':
+        $data['media'] = empty($data['media']) ? [] : $data['media'];
+        array_push($data['media'], extract_media_response($child));
+        break;
+
+      default:
+    }
+  }
+
+  return $data;
+}
+
+function extract_media_response($model) {
+  $data = [
+    'id' => (int) $model['id'],
+    // Images don't store external_keys yet.
+    'created_on' => $model['created_on'],
+    'updated_on' => $model['updated_on'],
+  ];
+
+  return $data;
+}
+
+function forward_post_to($entity, $submission = NULL, $writeTokens = NULL) {
+  $media = extract_media_data($_FILES);
+  $request = data_entry_helper::$base_url . "index.php/services/data/$entity";
+  $postargs = 'submission=' . urlencode(json_encode($submission));
+
+  // Pass through the authentication tokens as POST data.
+  foreach ($writeTokens as $token => $value) {
+    $postargs .= '&' . $token . '=' . ($value === TRUE ? 'true' : ($value === FALSE ? 'false' : $value));
+  }
+
+  $postargs .= '&user_id=' . hostsite_get_user_field('indicia_user_id');
+
+  // If there are images, we will send them after the main post,
+  // so we need to persist the write nonce.
+  if (count($media) > 0) {
+    $postargs .= '&persist_auth=true';
+  }
+  $response = data_entry_helper::http_post($request, $postargs, FALSE);
+
+  // The response should be in JSON if it worked.
+  $output = json_decode($response['output'], TRUE);
+
+  // If this is not JSON, it is an error, so just return it as is.
+  if (!$output) {
+    $output = $response['output'];
+  }
+
+  if (is_array($output) && array_key_exists('success', $output))  {
+    // Submission succeeded.
+    // So we also need to move the images to the final location.
+    $image_overall_success = TRUE;
+    $image_errors = array();
+    foreach ($media as $item) {
+      // No need to resend an existing image, or a media link, just local files.
+      if ((empty($item['media_type']) || preg_match('/:Local$/', $item['media_type'])) &&
+        empty($item['id'])) {
+        if (!isset(data_entry_helper::$final_image_folder) ||
+          data_entry_helper::$final_image_folder=='warehouse') {
+          // Final location is the Warehouse
+          // @todo Set PERSIST_AUTH false if last file
+          $success = send_file_to_warehouse($item['path'], TRUE, $writeTokens);
+        }
+        else {
+          $success = rename(
+            $interim_image_folder.$item['path'],
+            $final_image_folder.$item['path']
+          );
+        }
+
+        if ($success !== TRUE) {
+          // Record all files that fail to move successfully.
+          $image_overall_success = FALSE;
+          $image_errors[] = $success;
+        }
+      }
+    }
+    if (!$image_overall_success) {
+      // Report any file transfer failures.
+      $error = lang::get('submit ok but file transfer failed') . '<br/>';
+      $error .= implode('<br/>', $image_errors);
+      $output = array('error' => $error);
+    }
+  }
+  return $output;
+}
+
+function extract_media_data() {
+  $r = [];
+  foreach ($_FILES as $key => $file) {
+    if ($file['error'] == '1') {
+      // File too big error dur to php.ini setting.
+      if (data_entry_helper::$validation_errors === NULL) {
+        data_entry_helper::$validation_errors = array();
+      }
+      data_entry_helper::$validation_errors[$key] = lang::get('file too big for webserver');
+    }
+    elseif (!data_entry_helper::check_upload_size($file)) {
+      // Warehouse may still block it.
+      if (data_entry_helper::$validation_errors==NULL) data_entry_helper::$validation_errors = array();
+      data_entry_helper::$validation_errors[$key] = lang::get('file too big for warehouse');
+    }
+
+
+    $destination = $file['name'];
+    $interim_image_folder = isset(data_entry_helper::$interim_image_folder) ?
+      data_entry_helper::$interim_image_folder :
+      'upload/';
+    $uploadpath = data_entry_helper::relative_client_helper_path() . $interim_image_folder;
+
+    if (move_uploaded_file($file['tmp_name'], $uploadpath . $destination)) {
+      $r[] = array(
+        // Id is set only when saving over an existing record.
+        // This will always be a new record.
+        'id' => '',
+        'path' => $destination,
+        'caption' => '',
+      );
+      $pathField = str_replace(array(':medium',':image'), array('_medium:path','_image:path'), $key);
+      $_POST[$pathField] = $destination;
+    }
+
+  }
+  return $r;
+}
+
+/**
+ * Takes a file that has been uploaded to the client website upload folder, and moves it to the warehouse upload folder using the
+ * data services.
+ *
+ * @param string $path Path to the file to upload, relative to the interim image path folder (normally the
+ * client_helpers/upload folder.
+ * @param boolean $persist_auth Allows the write nonce to be preserved after sending the file, useful when several files
+ * are being uploaded.
+ * @param array $readAuth Read authorisation tokens, if not supplied then the $_POST array should contain them.
+ * @param string $service Path to the service URL used. Default is data/handle_media, but could be import/upload_csv.
+ * @return string Error message, or true if successful.
+ */
+function send_file_to_warehouse($path, $persist_auth=FALSE, $readAuth = NULL, $service='data/handle_media') {
+  if ($readAuth==NULL) $readAuth=$_POST;
+  $interim_image_folder = isset(data_entry_helper::$interim_image_folder) ?
+    data_entry_helper::$interim_image_folder :
+    'upload/';
+
+  $interim_path = data_entry_helper::relative_client_helper_path().$interim_image_folder;
+  if (!file_exists($interim_path.$path))
+    return "The file $interim_path$path does not exist and cannot be uploaded to the Warehouse.";
+
+  $serviceUrl = data_entry_helper::$base_url."index.php/services/".$service;
+  // This is used by the file box control which renames uploaded files using a guid system, so disable renaming on the server.
+  $postargs = array('name_is_guid' => 'true');
+  // Attach authentication details
+
+  if (array_key_exists('auth_token', $readAuth))
+    $postargs['auth_token'] = $readAuth['auth_token'];
+  if (array_key_exists('nonce', $readAuth))
+    $postargs['nonce'] = $readAuth['nonce'];
+  if ($persist_auth)
+    $postargs['persist_auth'] = 'true';
+
+  $file_to_upload = array('media_upload'=>'@'.realpath($interim_path.$path));
+  $response = data_entry_helper::http_post($serviceUrl, $file_to_upload + $postargs);
+  $output = json_decode($response['output'], TRUE);
+  $r = TRUE; // Default is success
+  if (is_array($output)) {
+    //an array signals an error
+    if (array_key_exists('error', $output)) {
+      // Return the most detailed bit of error information
+      if (isset($output['errors']['media_upload']))
+        $r = $output['errors']['media_upload'];
+      else
+        $r = $output['error'];
+    }
+  }
+  //remove local copy
+  unlink(realpath($interim_path.$path));
+  return $r;
 }
